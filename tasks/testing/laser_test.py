b27307f1dd034626c7240519516013deed4c2ded
guy <guykoy123@gmail.com>
2024-06-17 16:02:33 +0300

    new hardware tests

---
+++ tasks/testing/laser_test.py
@@ -0,0 +1,28 @@
+from pyControl.utility import *
+from devices import *
+
+board = Breakout_1_2()
+port_exp = Port_expander(port = board.port_3)
+laser=Digital_output(pin = port_exp.port_2.DIO_B)
+
+v.number_of_pulses=1
+v.pulse_duration=10
+v.inter_pulse_interval=100
+v.___started=False
+states = ['main_loop']
+initial_state = 'main_loop'
+events=['turn_on','turn_off']
+def main_loop(event):
+    if not v.___started:
+        v.___started=True
+        publish_event('turn_on')
+    if event=='turn_on':
+        laser.on()
+        v.number_of_pulses-=1
+        set_timer('turn_off',v.pulse_duration)
+    elif event=='turn_off':
+        laser.off()
+        if v.number_of_pulses>0:
+            set_timer('turn_on',v.inter_pulse_interval)        
+        else:
+            stop_framework()
---
+++ tasks/testing/lickometer_test.py
@@ -0,0 +1,115 @@
+"""
+this task allows control over each motor separately
+lab worker will input position in each axis in the variables window and set the new values
+then the lab worker must trigger the move event to move the motors to position
+return home event will make sure motors move to zero position even if position variable has incorrect data
+"""
+
+from pyControl.utility import *
+from devices import *
+
+board = Breakout_1_2()
+port_exp = Port_expander(port = board.port_3)
+lickometer = Lickometer(port_exp.port_1,debounce=20)
+motor_z = Stepper_motor(board.port_2)
+motor_y = Stepper_motor(port = board.port_5)
+motor_x = Stepper_motor(port = board.port_6)
+
+#public motor position variable, when different from private ones, move the motors
+v.motor_z_position=0
+v.motor_y_position=0
+v.motor_x_position=0
+
+v.motor_z_pos___=0
+v.motor_y_pos___=0
+v.motor_x_pos___=0
+
+v.motors_stationary___=True
+
+v.motor_speed = 1500
+
+states = ['moved','main']
+initial_state='main'
+events=['move','wait','return home','lick_1','lick_1_off']
+
+def move_motor_into_position(motor, position):
+    """
+    moves desired motor into position
+    and updates position in variables
+    in: 
+        motor (string) contains motor axis (x,y,z)
+        position (int) desired position in steps
+    out:
+        move (int) how many steps the motor need to move
+    """
+    move=0
+    if motor=='x':
+        move = position-v.motor_x_pos___
+        if move>0 and move<=4800:
+            motor_x.backward(v.motor_speed,move)
+            v.motor_x_pos___+=move
+        elif move>-4800 and move<0:
+            motor_x.forward(v.motor_speed,move*(-1))
+            v.motor_x_pos___+=move
+
+    elif motor=='y':
+        move = position-v.motor_y_pos___
+        if move>0 and move<=4800:
+            motor_y.backward(v.motor_speed,move)
+            v.motor_y_pos___+=move
+        elif move>=-4800 and move<0:
+            motor_y.forward(v.motor_speed,move*(-1))
+            v.motor_y_pos___+=move
+
+    elif motor=='z':
+        move = position-v.motor_z_pos___
+        if move>0 and move<=4800:
+            motor_z.backward(v.motor_speed,move)
+            v.motor_z_pos___+=move
+        elif move>=-4800 and move<0:
+            motor_z.forward(v.motor_speed,move*(-1))
+            v.motor_z_pos___+=move
+    return move
+
+
+def main(event):
+    if event=='move':
+        if v.motors_stationary___:
+            if v.motor_z_position != v.motor_z_pos___:
+                wait_time = move_motor_into_position('z',v.motor_z_position)
+                v.motors_stationary___=False
+                set_timer('wait',wait_time)
+            if v.motor_y_position != v.motor_y_pos___:
+                wait_time = move_motor_into_position('y',v.motor_y_position)
+                v.motors_stationary___=False
+                set_timer('wait',wait_time)
+            if v.motor_x_position != v.motor_x_pos___:
+                print("hit")
+                wait_time = move_motor_into_position('x',v.motor_x_position)
+                v.motors_stationary___=False
+                set_timer('wait',wait_time)
+                
+    if event=='wait':
+        v.motors_stationary___=True
+        print("motor in position")
+
+    if event =='return home':
+        motor_z.forward(1500,4800)
+        motor_y.forward(1500,4800)
+        motor_x.forward(1500,4800)
+        v.motor_z_position=0
+        v.motor_y_position=0
+        v.motor_x_position=0
+        set_timer('wait',6000*ms)
+
+            
+
+
+
+
+
+
+
+
+
+
